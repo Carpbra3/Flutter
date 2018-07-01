4a17d73daa84ae78158b3cb8c233b4a1365d2d74
Brandon Carpenter <Carpbra3@isu.edu>
2018-06-30 21:48:54 -0600

    hello rectangle app start
    Signed-off-by: Brandon Carpenter <Carpbra3@isu.edu>

---
+++ hello_rectangle/android/app/src/main/java/hellorectangle/brandondev/com/hellorectangle/MainActivity.java
@@ -0,0 +1,14 @@
+package hellorectangle.brandondev.com.hellorectangle;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
